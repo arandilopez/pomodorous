24d57681c144967054f6e8b5b2e1b97d4525128a
Arandi López <arandilopez.93@gmail.com>
2017-03-22 00:52:50 -0600

    Circle progress

--- app/src/renderer/components/MainPage.vue
+++ app/src/renderer/components/MainPage.vue
@@ -1,19 +1,31 @@
 <template>
-  <div>
-    <div class="columns">
-      <div class="column is-10 is-offset-1">
-      </div>
+  <div id="main-page">
+    <div id="container">
+      <timer></timer>
     </div>
   </div>
 </template>
 
 <script>
+import Timer from './Timer'
+
 export default {
   components: {
+    Timer
   },
   name: 'main-page'
 }
 </script>
 
 <style scoped>
+#main-page #container {
+  margin-top: 40px;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
---
+++ app/src/renderer/components/Timer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="#timer">
+    <radial-progress-bar :diameter="300"
+    :completed-steps="completedSteps"
+    :total-steps="totalSteps"
+    :start-color="startColor"
+    :stop-color="stopColor"
+    :inner-stroke-color="innerStrokeColor"
+    :strokeWidth="strokeWidth">
+      <h1 class="title is-3">Total steps: {{ totalSteps }}</h1>
+      <h3 class="subtitle is-5">Completed steps: {{ completedSteps }}</h3>
+    </radial-progress-bar>
+</div>
+</template>
+
+<script>
+import RadialProgressBar from 'vue-radial-progress/src/RadialProgressBar'
+
+export default {
+  data () {
+    return {
+      completedSteps: 750,
+      totalSteps: 1500,
+      startColor: '#589e79',
+      stopColor: '#960d41',
+      innerStrokeColor: '#c2c2c2',
+      strokeWidth: 5
+    }
+  },
+
+  components: {
+    RadialProgressBar
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+#timer {
+  margin: 0 auto;
+}
+</style>
