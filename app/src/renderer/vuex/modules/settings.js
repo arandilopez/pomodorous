4ba90bdad8ac889864a80575ee43cacddf9450de
Arandi López <arandilopez.93@gmail.com>
2017-03-23 12:34:23 -0600

    settings state and mutations

--- app/src/renderer/vuex/modules/settings.js
+++ app/src/renderer/vuex/modules/settings.js
@@ -1,10 +1,21 @@
-// import * as types from '../mutation-types'
+import * as types from '../mutation-types'
 
 const state = {
-  main: 0
+  cycleTime: 25,
+  longBrakeTime: 20,
+  shortBrakeTime: 5
 }
 
 const mutations = {
+  [types.SET_SHORT_BRAKE_TIME] (state, payload) {
+    state.shortBrakeTime = payload
+  },
+  [types.SET_LONG_BRAKE_TIME] (state, payload) {
+    state.longBrakeTime = payload
+  },
+  [types.SET_CYCLE_TIME] (state, payload) {
+    state.cycleTime = payload
+  }
 }
 
 export default {
